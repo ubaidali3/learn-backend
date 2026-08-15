9aca0e8a2126a3e3d2fffee2de2f286626869994
Ubaid Ali <uabidali074@gmail.com>
2026-08-15 14:42:38 +0500

    add new routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,25 +1,41 @@
-import {Router} from 'express'
-import { loginUser, logOutUser, refreshAccessToken, registerUser } from '../controllers/user.controllers.js'
-import {upload} from '../middlewares/mullter.middleware.js'
-import { verifyJWT } from '../middlewares/auth.middleware.js'
+import { Router } from "express";
+import {
+    changeCurrentPassword,
+    getCurrentUser,
+    getUserChannelProfile,
+    getWatchHistory,
+    loginUser,
+    logOutUser,
+    refreshAccessToken,
+    registerUser,
+    updateAccountDetails,
+    updateUserAvatar,
+} from "../controllers/user.controllers.js";
+import { upload } from "../middlewares/mullter.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
-const routes=Router()
+const routes = Router();
 
-routes.route('/register').post(
-  upload.fields([
-      {name:"avatar",
-        maxCount:1
+routes
+    .route("/register")
+    .post(upload.fields([{ name: "avatar", maxCount: 1 }]), registerUser);
+routes.route("/login").post(loginUser);
 
-      },
-      
-  ]),
-  registerUser)
-  routes.route('/login').post(loginUser)
+///sercure route
 
+routes.route("/logout").post(verifyJWT, logOutUser);
+routes.route("/refresh-token").post(refreshAccessToken);
+routes.route("/change-password").post(verifyJWT, changeCurrentPassword);
 
-  ///sercure route
+routes.route("/current-user").get(verifyJWT, getCurrentUser);
 
-  routes.route('/logout').post(verifyJWT, logOutUser)
-  routes.route('/refresh-token').post(refreshAccessToken)
+routes
+    .route("/avatar")
+    .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
 
-export default routes
+//// using parama
+routes.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+
+routes.route("/history").get(verifyJWT, getWatchHistory);
+
+export default routes;
